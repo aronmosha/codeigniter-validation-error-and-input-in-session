8b1c3ae834e6ea88840c00f88c6adcd39bc6a1e6
Aron Mosha <aronmosha@gmail.com>
2015-02-19 17:12:34 +0300

    adds MY_form_helper.php which overides core CI form helper

---
+++ helpers/MY_form_helper.php
@@ -0,0 +1,52 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( ! function_exists('set_val'))
+{
+    function set_value($name)
+    {
+    	$suffix='_input';
+        $CI =& get_instance();
+		 $CI->load->library('session');
+		return $CI->session->flashdata($name.$suffix);    
+	}   
+}
+if ( ! function_exists('get_error'))
+{
+    function set_error($name)
+    {
+    	$suffix='_error';
+        $CI =& get_instance();
+		 $CI->load->library('session');
+		return $CI->session->flashdata($name.$suffix);    
+	}   
+}
+if ( ! function_exists('set_radio'))
+{
+    function set_radio($name,$value)
+    {
+        $suffix='_input';
+        $CI =& get_instance();
+         $CI->load->library('session');
+        return ($CI->session->flashdata($name.$suffix)==$value)?"cheked":"";   
+    }   
+}
+if ( ! function_exists('set_select'))
+{
+    function set_select($name,$value)
+    {
+        $suffix='_input';
+        $CI =& get_instance();
+         $CI->load->library('session');
+        return ($CI->session->flashdata($name.$suffix)==$value)?"cheked":"";     
+    }   
+}
+if ( ! function_exists('set_checkbox'))
+{
+    function set_checkbox($name,$value)
+    {
+        $suffix='_input';
+        $CI =& get_instance();
+         $CI->load->library('session');
+        return ($CI->session->flashdata($name.$suffix)==$value)?"cheked":"";       
+    }   
+}

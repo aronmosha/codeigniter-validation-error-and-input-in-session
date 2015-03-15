662295566f8a5a0fd521e943a483c5b1294697e8
Aron Mosha <aronmosha@gmail.com>
2015-03-15 16:01:16 +0300

    adds default option on form value

--- helpers/MY_form_helper.php
+++ helpers/MY_form_helper.php
@@ -2,51 +2,75 @@
 
 if ( ! function_exists('set_value'))
 {
-    function set_value($name)
+    function set_value($name,$default='')
     {
-    	$suffix='_input';
+        $suffix='_input';
         $CI =& get_instance();
-		 $CI->load->library('session');
-		return $CI->session->flashdata($name.$suffix);    
-	}   
+         $CI->load->library('session');
+         $result=$CI->session->flashdata($name.$suffix);
+         if($result==FALSE){
+            return $default;
+         }
+         else{
+            return $result;
+         }    
+    }   
 }
 if ( ! function_exists('form_error'))
 {
     function form_error($name)
     {
-    	$suffix='_error';
+        $suffix='_error';
         $CI =& get_instance();
-		 $CI->load->library('session');
-		return $CI->session->flashdata($name.$suffix);    
-	}   
+        $CI->load->library('session');
+        return $CI->session->flashdata($name.$suffix);    
+    }   
 }
 if ( ! function_exists('set_radio'))
 {
-    function set_radio($name,$value)
+    function set_radio($name,$value,$default='')
     {
         $suffix='_input';
         $CI =& get_instance();
          $CI->load->library('session');
-        return ($CI->session->flashdata($name.$suffix)==$value)?"cheked":"";   
+         $result=$CI->session->flashdata($name.$suffix);
+         if($result==FALSE){
+            if($value==$default) return 'checked';
+         }
+         else{
+            if($result==$value) return 'checked';
+         }   
     }   
 }
 if ( ! function_exists('set_select'))
 {
-    function set_select($name,$value)
+    function set_select($name,$value,$default='')
     {
         $suffix='_input';
         $CI =& get_instance();
          $CI->load->library('session');
-        return ($CI->session->flashdata($name.$suffix)==$value)?"cheked":"";     
+         $result=$CI->session->flashdata($name.$suffix);
+         if($result==FALSE){
+            if($value==$default) return 'selected';
+         }
+         else{
+            if($result==$value) return 'selected';
+         }      
     }   
 }
 if ( ! function_exists('set_checkbox'))
 {
-    function set_checkbox($name,$value)
+    function set_checkbox($name,$value,$default='')
     {
         $suffix='_input';
         $CI =& get_instance();
          $CI->load->library('session');
-        return ($CI->session->flashdata($name.$suffix)==$value)?"cheked":"";       
+         $result=$CI->session->flashdata($name.$suffix);
+         if($result==FALSE){
+            if($value==$default) return 'checked';
+         }
+         else{
+            if($result==$value) return 'checked';
+         }       
     }   
 }
